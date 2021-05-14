55df0b76dc965cdc8187193bc4c2216cc6147183
anshithavelagapudi <anshithavelagapudi@gmail.com>
2021-05-14 18:05:43 -0400

    added htmlcss project

---
+++ htmlcss/script.js
@@ -0,0 +1,23 @@
+// let user="test";
+// function A(){
+//      user="new test";
+//     console.log(user);
+
+// }
+// console.log(user);
+// A();
+// console.log(user);
+
+
+function getKeyPress(){
+    console.log("keypress event");
+}
+
+function getKeyUp(){
+    console.log("keyup event");
+
+}
+
+function getKeyDown(){
+    console.log("keydown event");
+}
